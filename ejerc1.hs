a94dd8afcb87a75accd04f38268c2d6f51d76321
Ignacio Nievas <nachoniev@gmail.com>
2022-09-08 17:08:40 -0300

    ejerc2 hecho

--- ejerc1.hs
+++ ejerc1.hs
@@ -56,3 +56,38 @@ xor False False = False
 -- ejercicio-k
 swap1 (x,y) = (y,x) 
 
+
+
+
+--ejerc2:
+
+a :: (Int -> Int) -> Int
+a f = 1
+
+b :: Int -> (Int -> Int)
+b x y = x + y
+
+c :: (Int -> Int) -> (Int -> Int)
+c f = f
+
+d :: Int -> Bool
+d x | x >= 0 = True
+    | otherwise = False
+
+e :: Bool -> (Bool -> Bool)
+e a = \x -> True
+
+f :: (Int, Char) -> Bool
+f (a,b) = True
+
+g :: (Int,Int) -> Int
+g (a,b) = a+b
+
+h :: Int -> (Int,Int)
+h a = (a,a)
+
+i :: a -> Bool
+i x = True
+
+j :: a -> a
+j a = a
